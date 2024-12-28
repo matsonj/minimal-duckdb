05cde7c5dfa97459199da7158f0ceb0bb196dd2e
Jacob Matson <matsonj@gmail.com>
2024-12-27 17:37:24 -0700

    simplify test query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { queryAsync } from "@/lib/queryAsync";
 
 export default async function Home() {
-  const tables = await queryAsync("SHOW ALL TABLES");
+  const tables = await queryAsync("select * from jdw_dev.jdw_tpcds.query1 limit 1");
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
